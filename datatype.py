a359e2eb493160e33a05496bd299d4b2416f3c58
Charles Keith Felizardo <superck38@gmail.com>
2018-10-25 21:55:44 +0800

    Exercise No.1

---
+++ datatype.py
@@ -0,0 +1,11 @@
+age = 17
+weight = 40.00
+
+data = {"name": "Kit",
+"age": 17,"weight"
+: 34.34,"grades":[78,84,65,90]}
+
+#print(data)
+
+print ("I am {}".format(data["name"]))
+print ("My final grade in Filipino is {}".format(data["grades"][0]))
--- hello.py
+++ hello.py
@@ -1,7 +1,5 @@
 print("Hello,World!")
-<<<<<<< HEAD
 print("My name is {}. I am {} y/o."\
  .format("Kit",17))
-=======
-print("I am Kit")
->>>>>>> 1e8586f61270fc5fdb814545d7cd1cd3a69dd2c5
+print ("ADNU",2018, sep="-",end ="")
+print ("CSNHS")
